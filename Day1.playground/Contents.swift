27c658f54ac5381d9438b029b4034cf056b981af
Alperen Çerkez <alperencerkezz@gmail.com>
2024-08-13 14:02:21 +0300

    Initial commit

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var str1 = """
+This goes
+over multiple lines
+"""
+
+var str2 = """
+This goes \
+over multiple lines
+"""
+
+var score = 100
+var str = "Your score was \(score)"
+
+let multiLineString = """
+This is a multi-line string.
+It spans multiple lines.
+You can format it
+just like this.
+"""
+
+var city = "Istanbul"
+var name = "Alperen"
+var message = "Welcome to \(city) again \(name)!"
+
+let album: String = "Bir Pesimistin Gozyaslari"
+let year: Int = 2003
+let height: Double = 1.83
+let sagoDances: Bool = true
