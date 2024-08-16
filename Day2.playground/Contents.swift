3aff7d65e97c446bb729c151d999491c29c84564
Alperen Çerkez <alperencerkezz@gmail.com>
2024-08-16 14:23:06 +0300

    day2

---
+++ Day2.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let ferman = "Ferman Akgul"
+let yagmur = "Yagmur Sarigul"
+let cem = "Cem Bahtiyar"
+let ozgur = "Ozgur Can Oney"
+
+let maNga = [ferman, yagmur, cem, ozgur]
+
+maNga[0]
+
+let colors = Set(["red", "green", "blue"])
+
+var name = (first: "Selda", last: "Bagcan")
+name.0
+
+let heights = [
+    "Curry": 1.88,
+    "Shaq": 2.16
+]
+heights["Curry"]
+
+enum Result {
+    case success
+    case failure
+}
+
+let result = Result.success
