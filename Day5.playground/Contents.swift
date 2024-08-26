2639e4196a951a7c52f638738ca9b39b84a89f19
Alperen Çerkez <alperencerkezz@gmail.com>
2024-08-26 10:37:31 +0300

    day5

--- Day5.playground/Contents.swift
+++ Day5.playground/Contents.swift
@@ -38,4 +38,39 @@ func makeBand(names: [String]) {
 }
 makeBand(names: ["John", "Paul"])
 
+func square2(number: Int) -> Int {
+    return number * number
+}
+let result = square2(number: 8)
+print(result)
 
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+greet("Alperen")
+greet("Taylor", nicely: false)
+
+enum CatProblems: Error {
+    case notACat
+    case unfriendly
+}
+func strokeCat(_ name: String) throws {
+    if name == "Mr Bitey" {
+        throw CatProblems.unfriendly
+    } else if name == "Lassie" {
+        throw CatProblems.notACat
+    } else {
+        print("You stroked \(name).")
+    }
+}
+
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
+var myNum = 10
+doubleInPlace(number: &myNum)
+print("Number is: ", myNum)
