681bd762ac328b8adf7787dc238582b87d1ba7ed
Alperen Çerkez <alperencerkezz@gmail.com>
2024-09-04 22:21:39 +0300

    Day13

---
+++ Day13.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+print(name1 + " and " + name2)
+
+var name = "Alperen Cerkez"
+print("Your name is \(name)")
+
+var usbfan = "Heifer"
+print("I have a \(usbfan) USB Fan!")
+
+var person = ["first": "Taylor", "middle": "Alison", "last": "Swift", "month": "December", "website": "taylorswift.com"]
+person["middle"]
+person["month"]
+
+for i in 1...10 {
+    print("\(i) x 10 is \(i * 10)")
+}
+
