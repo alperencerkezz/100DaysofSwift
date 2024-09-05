a0c66ff2b6c3d60de291c316f0e18ddfe8e53b62
Alperen Çerkez <alperencerkezz@gmail.com>
2024-09-05 22:25:38 +0300

    day14

---
+++ Day14.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+func favoriteAlbum(name: String) {
+    print("My favorite is \(name)")
+}
+favoriteAlbum(name: "Fearless")
+
+func printAlbumRelease(name: String, year: Int) {
+    print("\(name) was released in \(year)")
+}
+
+printAlbumRelease(name: "Fearless", year: 2008)
+printAlbumRelease(name: "Speak Now", year: 2010)
+printAlbumRelease(name: "Red", year: 2012)
+
+
+
+struct Person {
+    var clothes: String
+    var shoes: String
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+
+var taylorCopy = taylor
+taylorCopy.shoes = "flip flops"
+
+print(taylor)
+print(taylorCopy)
