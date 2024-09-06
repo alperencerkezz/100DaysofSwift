1932d143e55d01d401e68525fe4c70c5bfc004ec
Alperen Çerkez <alperencerkezz@gmail.com>
2024-09-06 16:51:20 +0300

    day15

---
+++ Day15.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+struct Person {
+    var clothes: String
+    var shoes: String
+
+    func describe() {
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+taylor.describe()
+other.describe()
+
+struct TaylorFan {
+    static var favoriteSong = "Look What You Made Me Do"
+
+    var name: String
+    var age: Int
+}
+
+let fan = TaylorFan(name: "James", age: 25)
+print(TaylorFan.favoriteSong)
