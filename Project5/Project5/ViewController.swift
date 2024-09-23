4ada87b512b171f2e0db0da304edac91b7a36a1c
Alperen Çerkez <alperencerkezz@gmail.com>
2024-09-23 16:39:25 +0300

    challenges

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "New Word", style: .plain , target: self, action: #selector(startGame))
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
-
+        
         if let startsWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startsWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -23,43 +25,43 @@ class ViewController: UITableViewController {
         } else {
             allWords = ["silkworm"]
         }
-
+        
         startGame()
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
-
-    func startGame() {
+    
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
-
+    
     @objc func promptForAnswer() {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
-
+        
         let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] action in
             let answer = ac.textFields![0]
             self.submit(answer: answer.text!)
         }
-
+        
         ac.addAction(submitAction)
-
+        
         present(ac, animated: true)
     }
-
+    
     func isPossible(word: String) -> Bool {
         var tempWord = title!.lowercased()
-
+        
         for letter in word {
             if let pos = tempWord.range(of: String(letter)) {
                 tempWord.remove(at: pos.lowerBound)
@@ -67,52 +69,76 @@ class ViewController: UITableViewController {
                 return false
             }
         }
-
+        
         return true
     }
-
+    
     func isOriginal(word: String) -> Bool {
-        return !usedWords.contains(word)
+        return !usedWords.contains { $0.lowercased() == word.lowercased() }
     }
-
+    
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSMakeRange(0, word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-
+        
         return misspelledRange.location == NSNotFound
     }
-
-    func submit(answer: String) {
-        let lowerAnswer = answer.lowercased()
-
-        let errorTitle: String
-        let errorMessage: String
-
-        if isPossible(word: lowerAnswer) {
-            if isOriginal(word: lowerAnswer) {
-                if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
-
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-
-                    return
-                } else {
-                    errorTitle = "Word not recognised"
-                    errorMessage = "You can't just make them up, you know!"
-                }
-            } else {
-                errorTitle = "Word used already"
-                errorMessage = "Be more original!"
-            }
-        } else {
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from '\(title!.lowercased())'!"
-        }
-
+    
+    func isLongEnough(word: String) -> Bool {
+        return word.count >= 3
+    }
+    
+    func isNotStartWord(word: String) -> Bool {
+        return word.lowercased() != title?.lowercased()
+    }
+    func showErrorMessage(errorTitle: String, errorMessage: String) {
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        var errorTitle: String
+        var errorMessage: String
+        
+        if isNotStartWord(word: lowerAnswer) {
+            if isLongEnough(word: lowerAnswer) {
+                if isPossible(word: lowerAnswer) {
+                    if isOriginal(word: lowerAnswer) {
+                        if isReal(word: lowerAnswer) {
+                            // Everything is correct, so we can add the word
+                            usedWords.insert(answer, at: 0)
+                            
+                            let indexPath = IndexPath(row: 0, section: 0)
+                            tableView.insertRows(at: [indexPath], with: .automatic)
+                            
+                            return
+                        } else {
+                            errorTitle = "Word not recognized"
+                            errorMessage = "You can't just make them up, you know!"
+                        }
+                    } else {
+                        errorTitle = "Word used already"
+                        errorMessage = "Be more original!"
+                    }
+                } else {
+                    errorTitle = "Word not possible"
+                    errorMessage = "You can't spell that word from '\(title!.lowercased())'!"
+                }
+            } else {
+                errorTitle = "Word too short"
+                errorMessage = "Your word must be at least three letters long."
+            }
+        } else {
+            errorTitle = "Start word"
+            errorMessage = "You can't use the start word as your answer!"
+        }
+        
+        // Call the showErrorMessage method with the errorTitle and errorMessage
+        showErrorMessage(errorTitle: errorTitle, errorMessage: errorMessage)
     }
 }
