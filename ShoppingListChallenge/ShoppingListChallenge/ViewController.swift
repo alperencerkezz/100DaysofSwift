3ee1730e42eb765d82859350e677c5e91e58a96d
Alperen Çerkez <alperencerkezz@gmail.com>
2024-09-26 15:07:08 +0300

    shopping list app

---
+++ ShoppingListChallenge/ShoppingListChallenge/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  ShoppingListChallenge
+//
+//  Created by Alperen Çerkez on 26.09.2024.
+//
+
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Shopping List"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Clear", style: .plain, target: self, action: #selector(clearList))
+    }
+
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Add item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+
+    func submit(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+
+    @objc func clearList() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
+
+    @objc func shareList() {
+        let list = shoppingList.joined(separator: "\\n")
+        let vc = UIActivityViewController(activityItems: [list], applicationActivities: [])
+        present(vc, animated: true)
+    }
+}
