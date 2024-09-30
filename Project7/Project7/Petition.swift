7dfdd7eee308b48e63134cd38472af96ddfebd73
Alperen Çerkez <alperencerkezz@gmail.com>
2024-09-30 13:46:50 +0300

    added functionality to fetch and display petitions from an API, parsing JSON data into table view cells

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Alperen Çerkez on 30.09.2024.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
