decad7498e1ff80ce2fb87cb6d76eb07f03b059d
Alperen Çerkez <alperencerkezz@gmail.com>
2024-10-01 14:48:55 +0300

    load html string

---
+++ Project7/Project7/DetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by Alperen Çerkez on 1.10.2024.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let detailItem = detailItem else {return}
+        
+        let html = """
+        <html>
+        <head>
+        <meta name = "viewport" content="width=device-width, inital-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+        
+    }
+    
+}
