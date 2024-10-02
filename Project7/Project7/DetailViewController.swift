0b943f46dc2e31edded084d7223bfd7608f1b419
Alperen Çerkez <alperencerkezz@gmail.com>
2024-10-03 01:48:21 +0300

    challenges

--- Project7/Project7/DetailViewController.swift
+++ Project7/Project7/DetailViewController.swift
@@ -9,35 +9,35 @@ import UIKit
 import WebKit
 
 class DetailViewController: UIViewController {
-    
     var webView: WKWebView!
     var detailItem: Petition?
-    
+
     override func loadView() {
         webView = WKWebView()
         view = webView
     }
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let detailItem = detailItem else {return}
-        
+
+        guard let detailItem = detailItem else { return }
+
+        // Inject custom CSS into the HTML content
         let html = """
         <html>
         <head>
-        <meta name = "viewport" content="width=device-width, inital-scale=1">
-        <style> body { font-size: 150%; } </style>
+        <style>
+        body { font-size: 150%; color: darkgray; }
+        h1 { color: darkblue; }
+        </style>
         </head>
         <body>
-        \(detailItem.body)
+        <h1>\(detailItem.title)</h1>
+        <p>\(detailItem.body)</p>
         </body>
         </html>
         """
-        
+
         webView.loadHTMLString(html, baseURL: nil)
-        
     }
-    
 }
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
+import UIKit
+
 class ViewController: UITableViewController {
     var petitions = [Petition]()
     var filteredPetitions = [Petition]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Adding Credits button to the top right corner
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
         
+        // Adding Filter button to the top left corner
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(promptForFilter))
+        
+        // Load the petitions
+        loadPetitions()
+    }
+    
+    // MARK: - Load petitions function
+    func loadPetitions() {
         let urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
-          urlString  = "https://www.hackingwithswift.com/samples/petitions-1.json"
+            urlString  = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
-        
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
@@ -32,41 +43,63 @@ class ViewController: UITableViewController {
             }
         }
         
-       
-        
         showError()
-        
-        
     }
     
-    @objc func showCredits () {
+    // MARK: - Filter petitions function
+    @objc func promptForFilter() {
+        let ac = UIAlertController(title: "Filter", message: "Enter a keyword to filter petitions:", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let filter = ac?.textFields?[0].text else { return }
+            self?.filterPetitions(filter)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func filterPetitions(_ filter: String) {
+        filteredPetitions = petitions.filter { petition in
+            return petition.title.contains(filter) || petition.body.contains(filter)
+        }
+        tableView.reloadData()
+    }
+    
+    // MARK: - Show credits function
+    @objc func showCredits() {
         let alert = UIAlertController(title: "Credits", message: "Data is sourced from hackingwithswift.com", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
     
-    func showError () {
+    // MARK: - Show error function
+    func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
     
+    // MARK: - Parse JSON data
     func parse(json: Data) {
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            filteredPetitions = petitions
             tableView.reloadData()
         }
     }
     
+    // MARK: - Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -74,8 +107,8 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+
     }
 }
-
