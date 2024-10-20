7e5aae2739bbc3c87fbbd0c158147c96bcab90ac
Alperen Çerkez <alperencerkezz@gmail.com>
2024-10-20 21:00:42 +0300

    challenges

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -9,16 +9,16 @@ import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var people = [Person]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return people.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
             fatalError("Unable to dequeue PersonCell.")
@@ -26,9 +26,8 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = people[indexPath.item]
         
-        // Debugging
         print("Setting up cell for person: \(person.name)")
-
+        
         cell.name.text = person.name
         
         let path = getDocumentsDirectory().appendingPathComponent(person.image)
@@ -40,16 +39,42 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         cell.layer.cornerRadius = 7
         
         return cell
-
+        
+    }
+    
+    @objc func addNewPerson() {
+        let ac = UIAlertController(title: "Add New Person", message: nil, preferredStyle: .actionSheet)
+        
+        // Option to use Camera
+        ac.addAction(UIAlertAction(title: "Take Photo", style: .default) { [weak self] _ in
+            self?.presentImagePicker(sourceType: .camera)
+        })
+        
+        // Option to use Photo Library
+        ac.addAction(UIAlertAction(title: "Choose from Library", style: .default) { [weak self] _ in
+            self?.presentImagePicker(sourceType: .photoLibrary)
+        })
+        
+        // Cancel option
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
     }
 
-    @objc func addNewPerson() {
+    func presentImagePicker(sourceType: UIImagePickerController.SourceType) {
+        guard UIImagePickerController.isSourceTypeAvailable(sourceType) else {
+            // If the selected source is not available, return
+            return
+        }
+        
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
+        picker.sourceType = sourceType
         present(picker, animated: true)
     }
 
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
         
@@ -66,7 +91,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         dismiss(animated: true)
     }
-
+    
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
@@ -75,6 +100,23 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
+        let ac = UIAlertController(title: "Choose an action", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "Rename", style: .default) { [weak self] _ in
+            self?.renamePerson(person)
+        })
+        
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.deleteItems(at: [indexPath])
+        })
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func renamePerson(_ person: Person) {
         let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
@@ -83,7 +125,9 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
             person.name = newName
             self?.collectionView.reloadData()
         })
+        
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
         present(ac, animated: true)
     }
 }
