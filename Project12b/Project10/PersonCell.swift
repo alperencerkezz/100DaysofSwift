5065597094784df8d76c19a6bae1a0944432690f
Alperen Çerkez <alperencerkezz@gmail.com>
2024-10-24 22:02:26 +0300

    added project 12, 12a, 12b

---
+++ Project12b/Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Alperen Çerkez on 19.10.2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
