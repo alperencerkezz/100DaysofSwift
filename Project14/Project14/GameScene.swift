ebdf3eb28e79649fceeb4559bc21274314433c35
Alperen Çerkez <alperencerkezz@gmail.com>
2024-11-24 15:16:58 +0300

    added images and sounds

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Alperen Çerkez on 24.11.2024.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+    
+}
