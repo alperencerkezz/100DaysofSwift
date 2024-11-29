6fbdba21dda5060bf77182a0877e078665912aed
Alperen Çerkez <alperencerkezz@gmail.com>
2024-11-29 11:57:44 +0300

    up and running

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Alperen Çerkez on 29.11.2024.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+
+}
--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -5,13 +5,22 @@
 //  Created by Alperen Çerkez on 28.11.2024.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let istanbul = Capital(title: "Istanbul", coordinate: CLLocationCoordinate2D(latitude: 41.015137, longitude: 28.979530), info: "The only city in the world built on two continents")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the city of light")
+        
+        mapView.addAnnotations([istanbul, oslo, paris])
+        
+        
+        
     }
 
 
