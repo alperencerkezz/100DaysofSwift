1774f47201de6b3c166012b5ec1abcfbb776f348
Alperen Çerkez <alperencerkezz@gmail.com>
2024-12-03 19:51:41 +0300

    p1

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Alperen Çerkez on 3.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i).") 
+        }
+    }
+
+
+}
+
