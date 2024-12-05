254efcdf8e520b88c397f292197289410c98dab7
Alperen Çerkez <alperencerkezz@gmail.com>
2024-12-05 11:18:55 +0300

    setting up

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Alperen Çerkez on 5.12.2024.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
