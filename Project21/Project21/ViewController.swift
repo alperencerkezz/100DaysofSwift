1a19ec4eebe659b11d05491c9440163d5e731f70
Alperen Çerkez <alperencerkezz@gmail.com>
2024-12-06 13:59:32 +0300

    show different instances of UIAlertController

--- Project21/Project21/ViewController.swift
+++ Project21/Project21/ViewController.swift
@@ -8,7 +8,7 @@
 import UserNotifications
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
     }
     
     @objc func scheduleLocal() {
+        registerCategories()
+        
         let center = UNUserNotificationCenter.current()
         center.removeAllPendingNotificationRequests()
         
@@ -50,7 +52,47 @@ class ViewController: UIViewController {
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
     }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [], options: [])
+        
+        center.setNotificationCategories([category])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
 
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
 
+            var title: String
+            var message: String
+
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                title = "Default Action"
+                message = "You interacted with the notification in the default way."
+
+            case "show":
+                title = "Show More Information"
+                message = "Here's more information about the custom data: \(customData)"
+
+            default:
+                title = "Unknown Action"
+                message = "An unknown action was triggered."
+            }
+
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true)
+            }
+        }
+
+        completionHandler()
+    }
 }
 
